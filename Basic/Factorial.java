a8145f54994870c95c0a2876e2305de82d99b4af
Rohit <flux.amv25@gmail.com>
2024-04-16 07:18:01 +0530

    Added Factorial.java

---
+++ Basic/Factorial.java
@@ -0,0 +1,19 @@
+package BasicMethods;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial = 1;
+        System.out.print("Factorial of "+num+" is ");
+        while(num>1){
+            int temp = num;
+            num = num-1;
+            factorial = temp*factorial;
+            System.out.print(temp +" * ");
+        }
+        System.out.print("1 = "+factorial);
+    }
+}
