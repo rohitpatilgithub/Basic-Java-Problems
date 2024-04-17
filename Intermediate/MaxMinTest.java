7d12903b1ee2bf6e82899becb3fc5ba386be5ae1
Rohit <flux.amv25@gmail.com>
2024-04-17 14:55:35 +0530

    Added new files

---
+++ Intermediate/MaxMinTest.java
@@ -0,0 +1,33 @@
+package BasicMethods.Methods;
+
+import java.util.Scanner;
+
+class Max{
+    Scanner sc = new Scanner(System.in);
+    int a;
+    int b;
+    int c;
+    void maxis(){
+        System.out.print("Enter your number : ");
+        a = sc.nextInt();
+        System.out.print("Enter your number : ");
+        b = sc.nextInt();
+        System.out.print("Enter your number : ");
+        c = sc.nextInt();
+        int max = Math.max(a,b);
+        int max1 = Math.max(max,c);
+        System.out.println("Maximum number is : "+max1);
+    }
+    void minis(){
+        int min = Math.min(a,b);
+        int min1 = Math.min(min,c);
+        System.out.println("Minimum number is : "+min1);
+    }
+}
+public class MaxMinTest {
+    public static void main(String[] args) {
+        Max ok = new Max();
+        ok.maxis();
+        ok.minis();
+    }
+}
---
+++ Intermediate/PrimeTest.java
@@ -0,0 +1,33 @@
+package BasicMethods.Methods;
+
+import java.util.Scanner;
+
+//Hint: Let n be the number to be checked. all the numbers from 0 to n are to be used to divide n.
+// If and only if the remainder is zero for division with n and 1 then the number is prime, else not
+
+class Prime{
+    Scanner sc = new Scanner(System.in);
+    int num;
+    int count = 0;
+    void isPrime(){
+        System.out.print("Enter your number : ");
+        num = sc.nextInt();
+        for(int i = 1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
+        }
+        if(count<=2){
+            System.out.println("It is prime number.");
+        }
+        else {
+            System.out.println("It is not prime number.");
+        }
+    }
+}
+public class PrimeTest {
+    public static void main(String[] args) {
+        Prime ok = new Prime();
+        ok.isPrime();
+    }
+}
